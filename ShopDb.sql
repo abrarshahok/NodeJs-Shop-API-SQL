0176481a4b13fea7bc46b47f944bd001a116cf79
Abrar Ahmed Shahok <abrarahmedshahok12@gmail.com>
2024-05-24 22:13:31 +0500

    Completed Practicing SQL]

--- ShopDb.sql
+++ ShopDb.sql
@@ -8,10 +8,10 @@ CREATE TABLE Products (
     description TEXT
 );
 
-INSERT INTO Products (id, name, price, description) VALUES
-('1712427721577', 'Iphone 12 Pro Max', 210000, 'This is new mobile'),
-('1712429416384', 'Iphone 11 Pro Max', 180000, 'This is new mobile'),
-('1714416450413', 'Vivo S1', 30000, 'This is new mobile'),
-('1714568589888', 'Iphone 13 Pro Max', 250000, 'This is new mobile');
+-- INSERT INTO Products (id, name, price, description) VALUES
+-- ('1712427721577', 'Iphone 12 Pro Max', 210000, 'This is new mobile'),
+-- ('1712429416384', 'Iphone 11 Pro Max', 180000, 'This is new mobile'),
+-- ('1714416450413', 'Vivo S1', 30000, 'This is new mobile'),
+-- ('1714568589888', 'Iphone 13 Pro Max', 250000, 'This is new mobile');
 
 select * from Products;
