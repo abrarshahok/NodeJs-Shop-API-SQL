d6007cdf33c00c051f0714c9bef1f9d113df71fd
Abrar Ahmed Shahok <abrarahmedshahok12@gmail.com>
2024-05-30 00:05:01 +0500

    Some Fixes

--- src/middlewares/is-auth.js
+++ src/middlewares/is-auth.js
@@ -2,7 +2,7 @@ const isAuth = (req, res, next) => {
   if (!req.session.isLoggedIn) {
     return res.send({
       success: false,
-      message: `Please Login First`,
+      message: "Please Login First",
     });
   }
   next();
